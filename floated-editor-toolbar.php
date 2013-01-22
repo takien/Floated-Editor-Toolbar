cefe867307bd0e67b6dbb51a2abaa7f34c6121cd
takien <imissuaja@yahoo.com>
2013-01-22 13:26:08 +0700

    init

---
+++ floated-editor-toolbar.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Floated Editor Toolbar
+Author: Takien
+Version: 0.1
+Author URI: http://takien.com/
+
+*/
+defined('ABSPATH') OR die();
+
+add_action('admin_head','takien_floating_toolbar');
+function takien_floating_toolbar() {
+global $post_type;
+if('post' == $post_type) {
+echo '<style type="text/css">
+#content_toolbargroup.takien_floating_toolbar {
+	width:100%;
+	position:fixed;
+	background:#e4e4e4;
+	z-index:100000;
+	top:0;
+	left:0;
+	width:100%;
+}
+.takien_floating_toolbar table.mceToolbar{
+	float:left;
+}
+</style>';
+?>
+<script type="text/javascript">
+jQuery(document).ready(function($){
+	$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+		if(scroll < $('#content_parent').offset().top) {
+			$('#content_toolbargroup').removeClass('takien_floating_toolbar');
+		}
+		else {
+			$('#content_toolbargroup').addClass('takien_floating_toolbar');
+		}
+	});
+});
+
+</script>
+<?php
+}
+}
