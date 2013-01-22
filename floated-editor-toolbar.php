704d6f1905be77da02c771c5ccb17e6f6ac47a0c
takien <imissuaja@yahoo.com>
2013-01-22 14:15:58 +0700

    Add support for HTML/text mode

--- floated-editor-toolbar.php
+++ floated-editor-toolbar.php
@@ -8,12 +8,14 @@ Author URI: http://takien.com/
 */
 defined('ABSPATH') OR die();
 
+
 add_action('admin_head','takien_floating_toolbar');
 function takien_floating_toolbar() {
 global $post_type;
 if('post' == $post_type) {
 echo '<style type="text/css">
-#content_toolbargroup.takien_floating_toolbar {
+#content_toolbargroup.takien_floating_toolbar,
+#ed_toolbar.takien_floating_toolbar {
 	width:100%;
 	position:fixed;
 	background:#e4e4e4;
@@ -31,11 +33,11 @@ echo '<style type="text/css">
 jQuery(document).ready(function($){
 	$(window).scroll(function(){
 	var scroll = $(window).scrollTop();
-		if(scroll < $('#content_parent').offset().top) {
-			$('#content_toolbargroup').removeClass('takien_floating_toolbar');
+		if(scroll < $('#content_parent,#wp-content-editor-container').offset().top) {
+			$('#content_toolbargroup,#ed_toolbar').removeClass('takien_floating_toolbar');
 		}
 		else {
-			$('#content_toolbargroup').addClass('takien_floating_toolbar');
+			$('#content_toolbargroup,#ed_toolbar').addClass('takien_floating_toolbar');
 		}
 	});
 });
